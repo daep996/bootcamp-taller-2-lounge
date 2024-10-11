250963fed7a13d49b22d6bcb9bf537b26eb1b5b2
destra2@bancodebogota.com.co <destra2@bancodebogota.com.co>
2024-10-10 21:19:44 -0500

    add store

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,36 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setData } from '../store/dataSlice'
+
+
 const Home = () => {
+
+    const data = useSelector((state) => state.dataFetch.data);
+    const dispatch = useDispatch();
+    const url = 'https://fakestoreapi.com/products'
+
+    useEffect(() => {
+        fetch(url)
+            .then((response) => response.json())
+            .then((dataAPI) => {
+                console.log('results: ', dataAPI)
+                dispatch(setData(dataAPI))
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }, []);
+
     return (
         <div>
             <h1>Home</h1>
-            <p>Welcome to the home page</p>
+            <p>
+            {data.length > 0 && data.map((producto) => {
+                return (
+                    <p key={producto.id}>{producto.id} - {producto.title}</p>
+                )
+            })}
+            </p>
         </div>
     )
 }
