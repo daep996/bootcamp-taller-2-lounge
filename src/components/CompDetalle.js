e78ed26c768d407ef928049f4d44c440061a147d
fimercadog <fimercadog@gmail.com>
2024-10-10 20:07:06 -0500

    componentes detalle

---
+++ src/components/CompDetalle.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+function CompDetalle() {
+
+    return (
+
+        <div>
+        <h1>detalle </h1>
+        </div>
+
+    )
+}
+
+export default CompDetalle
+
