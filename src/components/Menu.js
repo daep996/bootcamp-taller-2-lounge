f88d95b38e7b2e01630b2e7aee7fd38be58122a6
Milton <milkthon@hotmail.com>
2024-10-11 20:17:53 -0500

    ultim

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,7 +6,7 @@ const Menu = () => {
     return (
         <>
             <nav className ="navbar navbar-expand-lg bg-body-tertiary">
-                <NavLink className={({ isActive }) => (isActive ? "navbar-brand" : null)} to='/'>Home</NavLink>
+                <NavLink className={({ isActive }) => (isActive ? "navbar-brand" : null)} to='/'>Home</NavLink>ln\
                 <NavLink className={({ isActive }) => (isActive ? "nav-link active" : null)} to='/galeria'>Galeria</NavLink>
                 <NavLink className={({ isActive }) => (isActive ? "nav-link active" : null)} to='detalle-producto'>Detalle-Producto</NavLink>
                 <NavLink className={({ isActive }) => (isActive ? "nav-link active" : null)} to='contacto'>Contacto</NavLink>
